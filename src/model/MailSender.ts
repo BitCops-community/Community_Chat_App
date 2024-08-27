3eed76cc2b05a1746a3bb37bad2889652787f3a6
BitCops-community <admin@bitcops.net>
2024-08-27 11:28:49 -0700

    Update MailSender.ts

--- src/model/MailSender.ts
+++ src/model/MailSender.ts
@@ -9,7 +9,13 @@ secure: true, // use SSL
     pass: process.env.NODEMAILER_PASSWORD!,
   },
 });
-
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
 async function main(receiverEmail: string, verificationLink: string) {
   console.log(`Sending Verification Email : ${receiverEmail}`);
   const htmlContent = `

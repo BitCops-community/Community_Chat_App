979399d38b46ce63130a688be093caac6fd46a08
BitCops-community <admin@bitcops.net>
2024-08-28 02:34:45 -0700

    Added Secure Admin

--- src/app/api/v1/users/admin/appoint/route.ts
+++ src/app/api/v1/users/admin/appoint/route.ts
@@ -29,11 +29,12 @@ const handler = async (req: NextRequest) => {
     if (currentUserId === userId) {
       return NextResponse.json({
         success: false,
-        message: "You cannot promote yourself to admin role",
+        message: "You cannot remove yourself from admin role",
       });
     }
 
     let currentUser = await User.findById(currentUserId);
+    console.log(currentUser);
 
     if (!currentUser) {
       return NextResponse.json({
@@ -41,7 +42,6 @@ const handler = async (req: NextRequest) => {
         message: "Current user not found",
       });
     }
-
     if (!currentUser.isAdmin) {
       return NextResponse.json({
         success: false,
@@ -56,13 +56,41 @@ const handler = async (req: NextRequest) => {
         message: "Invalid Request Invalid User!",
       });
     }
+    if (
+      newUser.username === "faisal_ranjha" ||
+      newUser.email === "faisalshahzadyt@gmail.com" ||
+      newUser.email === "ranjhaplaysyt@gmail.com"
+    ) {
+      return NextResponse.json({
+        success: false,
+        message: "You Cannot Remove this user From Admin Role",
+      });
+    }
+    // Check if the newUser is an admin that should be protected
+    if (
+      newUser.email === process.env?.ADMIN_EMAIL ||
+      newUser.username === process.env?.ADMIN_USERNAME
+    ) {
+      // Ensure only authorized users can remove the admin role
+      if (
+        currentUser.email !== "faisalshahzadyt@gmail.com" &&
+        currentUser.email !== "ranjhaplaysyt@gmail.com"
+      ) {
+        return NextResponse.json({
+          success: false,
+          message:
+            "You are not authorized to remove this user from the admin role",
+        });
+      }
+    }
 
-    newUser.isAdmin = true;
+    // If authorized, proceed to remove the admin role
+    newUser.isAdmin = false;
     await newUser.save();
 
     return NextResponse.json({
       success: true,
-      message: `Mr ${newUser?.name} Appointed As Admin`,
+      message: `Mr ${newUser?.name} Removed From Admin Role`,
     });
   } catch (error) {
     console.log(error);
